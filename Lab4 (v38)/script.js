790d6cc556e31184216ef2bb65caaacf5aab17fc
Ivona Josipovic <josipovic.ivona@gmail.com>
2022-09-19 12:54:03 +0200

    Uppdated images & href

---
+++ Lab4 (v38)/script.js
@@ -0,0 +1,17 @@
+let firstNum = 5;
+let secondNum = 10;
+
+console.log(firstNum + secondNum);
+console.log(firstNum - secondNum);
+console.log(firstNum * secondNum);
+
+console.log(`${firstNum} + ${secondNum} is: ${firstNum + secondNum}`)
+
+let obj = {
+  name: 'Jane',
+  lastName: 'Doe',
+  age: 30,
+}
+
+console.log(`Hi, my name is ${obj.name} ${obj.lastName} and I am ${obj.age} years old.`)
+
