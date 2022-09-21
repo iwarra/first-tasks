ece677b0c108cd03578980202160a0aff225124b
Ivona Josipovic <josipovic.ivona@gmail.com>
2022-09-21 16:08:24 +0200

    Adding Lab5

---
+++ Lab5 (v38)/script.js
@@ -0,0 +1,15 @@
+let names = ['Anna', 'Jane', 'Joe', 'Bob', 'Sarah'];
+
+names.pop();
+
+names[1] = 'Sam';
+
+names.push('Joakim');
+
+names.sort();
+
+names.indexOf('Joakim');
+
+names.splice(2,1);
+
+names.forEach(name => console.log(name));
