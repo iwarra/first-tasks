1955e363f11408dfabf6ac8fb219d1c0aca46c95
Ivona Josipovic <josipovic.ivona@gmail.com>
2022-09-23 12:51:52 +0200

    Adding Lab6

---
+++ Lab6 (v38)/script.js
@@ -0,0 +1,26 @@
+// Funktion 1:
+const hello = () => console.log('Hello world');
+
+hello();
+
+
+//Funktion 2:
+const greet = (name) => console.log('Hej ' + name + ' hur mår du?');
+
+greet('John');
+
+
+//Funktion 3:
+const calc = (numberOne, numberTwo) => {
+return numberOne + numberTwo
+};
+
+console.log(calc(1, 10));
+
+//Funktion 4:
+const tip = (sum, percent) => {
+let total = sum + sum / percent;
+return total;
+};
+
+console.log(tip(200, 10));
