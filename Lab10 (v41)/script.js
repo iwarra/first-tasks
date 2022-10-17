7aad82096b037d9ee7e3326455a00c889a9acf8f
Iwarra <josipovic.ivona@gmail.com>
2022-10-17 09:46:26 +0200

    Fixing js files

--- Lab10 (v41)/script.js
+++ Lab10 (v41)/script.js
@@ -1,18 +1,6 @@
-let imgDiv = document.querySelector('.imageDiv');
+let img = document.createElement('img');
+let imgDiv = document.querySelector('.image');
 let btn = document.querySelector('.btn');
-
-function stylingButton() {
-  let btnText = document.querySelector('.btn-text');
-  btnText.classList.add('after')
-}
-
-async function getDeck(nrOfDecks) {
-  let deck = await fetch ('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=' + nrOfDecks);
-  let deckJson = await deck.json();
-  console.log(deckJson)
-}
-
-getDeck(1)
 
 async function getCardJson() {
   let card = await fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=1');
@@ -20,27 +8,14 @@ async function getCardJson() {
   return cardJSON
 }
 
-async function showCards() {
-  let img = document.createElement('img');
-  img.classList.add('image');
-  
-  let cardJson = await getCardJson();
-  let cardImgURL = cardJson.cards.at(0).image;
-  img.setAttribute('src', cardImgURL);
+async function showCard() {
+  let cardURL = await getCardJson()
+  let card = cardURL.cards.at(0).image
+  img.setAttribute('src', card);
   imgDiv.appendChild(img)
 
-  stylingButton();
+  let btnText = document.querySelector('.btn-text');
+  btnText.classList.add('after')
 }
 
-async function showCardValue() {
-  let cardJson = await getCardJson()
-  let cardValue = cardJson.cards.at(0).value;
-  console.log(cardValue)
-}
-
-showCardValue()
-
-btn.addEventListener('click', () => { 
-  showCards()
- //showCardValue()
-})
+btn.addEventListener('click', showCard)
--- Lab10.b (v41)/script.js
+++ Lab10.b (v41)/script.js
@@ -1,6 +1,20 @@
-let img = document.createElement('img');
-let imgDiv = document.querySelector('.image');
+let imgDiv = document.querySelector('.imageDiv');
 let btn = document.querySelector('.btn');
+let resultText = document.querySelector('.result-text');
+let result;
+
+function stylingButton() {
+  let btnText = document.querySelector('.btn-text');
+  btnText.classList.add('after')
+}
+
+async function getDeck(nrOfDecks) {
+  let deck = await fetch ('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=' + nrOfDecks);
+  let deckJson = await deck.json();
+  //console.log(deckJson)
+}
+
+getDeck(1)
 
 async function getCardJson() {
   let card = await fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=1');
@@ -8,14 +22,32 @@ async function getCardJson() {
   return cardJSON
 }
 
-async function showCard() {
-  let cardURL = await getCardJson()
-  let card = cardURL.cards.at(0).image
-  img.setAttribute('src', card);
+async function showCards() {
+  let img = document.createElement('img');
+  img.classList.add('image');
+  
+  let cardJson = await getCardJson();
+  //console.log(cardJson)
+  let cardImgURL = cardJson.cards.at(0).image;
+  //console.log(cardImgURL)
+  img.setAttribute('src', cardImgURL);
   imgDiv.appendChild(img)
 
-  let btnText = document.querySelector('.btn-text');
-  btnText.classList.add('after')
+  stylingButton();
 }
 
-btn.addEventListener('click', showCard)
+async function showCardValue() {
+  let cardJson = await getCardJson()
+  let cardValue = cardJson.cards.at(0).value;
+  let result =+ cardValue;
+  console.log(result);
+  console.log(resultText.innerText)
+  resultText.innerText += result
+}
+
+showCardValue()
+
+btn.addEventListener('click', () => { 
+  showCards()
+ //showCardValue()
+})
