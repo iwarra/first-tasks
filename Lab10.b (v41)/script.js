2c1beab69f7a12d547c4f6968ce2e5612d439e0c
Iwarra <josipovic.ivona@gmail.com>
2022-10-17 18:36:16 +0200

    changes on currentCard func

--- Lab10.b (v41)/script.js
+++ Lab10.b (v41)/script.js
@@ -1,7 +1,7 @@
 let imgDiv = document.querySelector('.imageDiv');
 let btn = document.querySelector('.btn');
 let resultText = document.querySelector('.result-text');
-let result;
+let result = 0;
 
 function stylingButton() {
   let btnText = document.querySelector('.btn-text');
@@ -12,42 +12,65 @@ async function getDeck(nrOfDecks) {
   let deck = await fetch ('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=' + nrOfDecks);
   let deckJson = await deck.json();
   deckID = deckJson.deck_id;
+  console.log(deckID)
   return deckID
 }
 
 async function getCardJson() {
-  let card = await fetch(`https://deckofcardsapi.com/api/deck/${await getDeck(1)}/draw/?count=1`);
+  let deckID = await getDeck(1);
+  let card = await fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`);
   let cardJSON = await card.json();
   return cardJSON
 }
 
-async function showCards() {
+
+async function currentCard() {
+
+  let cardJson = await getCardJson();
+
+function showCards() {
   let img = document.createElement('img');
   img.classList.add('image');
-  
-  let cardJson = await getCardJson();
-  console.log(cardJson)
   let cardImgURL = cardJson.cards.at(0).image;
-  //console.log(cardImgURL)
   img.setAttribute('src', cardImgURL);
   imgDiv.appendChild(img)
 
   stylingButton();
 }
 
-async function showCardValue() {
-  let cardJson = await getCardJson()
+function showCardValue() {
   let cardValue = cardJson.cards[0].value;
-  console.log(cardValue)
-  let result =+ cardValue;
-  //console.log(result);
-  //console.log(resultText.innerText)
+
+  switch (true) {
+    case cardValue > 1 && cardValue <= 10:
+    cardValue = cardValue;
+    break;
+    case cardValue == 'JACK':
+    case cardValue == 'KING':
+    case cardValue == 'QUEEN':
+    cardValue = 10;
+    break;
+    case cardValue == 'ACE':
+    result > 12 ? cardValue = 1 : cardValue = 11;
+    default:
+      console.log('Chose your card.')
+  }
+
+  result = result + +cardValue;
   resultText.innerHTML = 'Your result is: ' + result;
+  return result;
 }
 
-//showCardValue()
+function loseOrWin() {
+  if (result == 21) alert('Congrats, you won!');
+  //if (result )
+}
+
+showCards();
+showCardValue()
+}
+
 
 btn.addEventListener('click', () => { 
-  showCards();
-  showCardValue()
+  currentCard()
 })
